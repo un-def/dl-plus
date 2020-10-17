064730c5a2b70035dea8aeec9fda4a00317fd79a
un.def <un.def@ya.ru>
2020-10-17 16:44:49 +0300

    Add ExtractorPlugin machinery

--- src/dl_plus/core.py
+++ src/dl_plus/core.py
@@ -8,10 +8,9 @@ import re
 from .exceptions import UnknownExtractor
 
 
-# PEQN - Pluggable Extractor Qualified Name
-PEQN_REGEX = re.compile(
-    r'^(?P<ns>[a-z0-9]+)/(?P<plugin>[a-z0-9]+)(?::(?P<extractor>[a-z0-9]+))?$')
 PLUGINS_PACKAGE = 'dl_plus.extractors'
+
+_NOTSET = object()
 
 
 def get_ie_name(ie_cls):
@@ -37,46 +36,103 @@ def discover_extractor_plugins_gen():
                 yield f'{PLUGINS_PACKAGE}.{ns}.{name}'
 
 
-def parse_peqn(peqn):
+class PEQN:
     """
-    Parse PEQN. Returns a pair:
-        * the plugin import path
-        * the extractor name or None
+    Pluggable Extractor Qualified Name
     """
-    match = PEQN_REGEX.fullmatch(peqn)
-    if not match:
-        raise UnknownExtractor('bad PEQN')
-    ns, plugin, extractor = match.group('ns', 'plugin', 'extractor')
-    if ns[0].isdigit():
-        ns = '_' + ns
-    if plugin[0].isdigit():
-        plugin = '_' + plugin
-    return f'{PLUGINS_PACKAGE}.{ns}.{plugin}', extractor
+
+    PLUGIN_IMPORT_PATH_PREFIX = f'{PLUGINS_PACKAGE}.'
+    PEQN_REGEX = re.compile(
+        r'^(?P<ns>[a-z0-9]+)/(?P<plugin>[a-z0-9]+)'
+        r'(?::(?P<name>[a-z0-9]+))?$'
+    )
+
+    def __init__(self, ns, plugin, name=None):
+        self._ns = ns
+        self._plugin = plugin
+        self._name = name
+
+    @classmethod
+    def from_plugin_import_path(cls, path):
+        if not path.startswith(cls.PLUGIN_IMPORT_PATH_PREFIX):
+            raise ValueError(f'bad plugin import path: {path}')
+        parts = path[len(cls.PLUGIN_IMPORT_PATH_PREFIX):].split('.')
+        if len(parts) != 2:
+            raise ValueError(f'bad plugin import path: {path}')
+        ns = cls._unescape_part(parts[0])
+        plugin = cls._unescape_part(parts[1])
+        return cls(ns, plugin)
+
+    @classmethod
+    def from_string(cls, peqn):
+        match = cls.PEQN_REGEX.fullmatch(peqn)
+        if not match:
+            raise ValueError(f'bad PEQN: {peqn}')
+        return cls(**match.groupdict())
+
+    def copy(self, ns=_NOTSET, plugin=_NOTSET, name=_NOTSET):
+        if ns is _NOTSET:
+            ns = self._ns
+        if plugin is _NOTSET:
+            plugin = self._plugin
+        if name is _NOTSET:
+            name = self._name
+        return self.__class__(ns, plugin, name)
+
+    def __str__(self):
+        if self._name:
+            return f'{self._ns}/{self._plugin}:{self._name}'
+        return f'{self._ns}/{self._plugin}'
+
+    @property
+    def ns(self):
+        return self._ns
+
+    @property
+    def plugin(self):
+        return self._plugin
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def plugin_import_path(self):
+        ns = self._escape_part(self._ns)
+        plugin = self._escape_part(self._plugin)
+        return f'{PLUGINS_PACKAGE}.{ns}.{plugin}'
+
+    @staticmethod
+    def _escape_part(part):
+        if part[0].isdigit():
+            return '_' + part
+        return part
+
+    @staticmethod
+    def _unescape_part(part):
+        if part[0] == '_':
+            return part[1:]
+        return part
 
 
-def load_extractors_from_plugin(plugin_path, extractors_names=None):
+def load_extractors_from_plugin(plugin_import_path, names=None):
     try:
-        module = importlib.import_module(plugin_path)
+        plugin_module = importlib.import_module(plugin_import_path)
     except ImportError:
-        raise UnknownExtractor(f'cannot import {plugin_path}')
+        raise UnknownExtractor(f'cannot import {plugin_import_path}')
     try:
-        extractor = module.EXTRACTOR
+        plugin = plugin_module.plugin
     except AttributeError:
         raise UnknownExtractor(
-            f'{plugin_path} does not export any extractor')
-    if isinstance(extractor, dict):
-        if extractors_names:
-            try:
-                extractors = [extractor[name] for name in extractors_names]
-            except KeyError as exc:
-                raise UnknownExtractor(
-                    f'{plugin_path} does not export {exc.args[0]}')
-        else:
-            extractors = list(extractor.values())
-    elif extractors_names:
-        raise UnknownExtractor(f'{plugin_path} has only one extractor')
+            f'{plugin_import_path} does not marked as an extractor plugin')
+    if names:
+        try:
+            extractors = list(map(plugin.get_extractor, names))
+        except KeyError as exc:
+            raise UnknownExtractor(
+                f'{plugin_import_path} does not export {exc.args[0]}')
     else:
-        extractors = [extractor]
+        extractors = plugin.get_all_extractors()
     return extractors
 
 
@@ -101,13 +157,16 @@ def enable_extractors(names):
                 )
             ))
         elif '/' in name:
-            plugin_path, extractor_name = parse_peqn(name)
-            if extractor_name:
-                extractors_names = [extractor_name]
+            try:
+                peqn = PEQN.from_string(name)
+            except ValueError:
+                raise UnknownExtractor(f'failed to parse PEQN: {name}')
+            if peqn.name:
+                names = [peqn.name]
             else:
-                extractors_names = None
+                names = None
             extractors = load_extractors_from_plugin(
-                plugin_path, extractors_names)
+                peqn.plugin_import_path, names)
         else:
             try:
                 extractors = [ytdl_ie_name_cls_map[name]]
--- src/dl_plus/extractor.py
+++ src/dl_plus/extractor.py
@@ -1,8 +1,64 @@
-import youtube_dl.extractor.common
+from functools import partial
+
+from youtube_dl.extractor.common import InfoExtractor
+
+from .core import PEQN
+from .exceptions import DLPlusException
 
 
-class InfoExtractor(youtube_dl.extractor.common.InfoExtractor):
+class ExtractorPluginError(DLPlusException):
+
+    pass
+
+
+class Extractor(InfoExtractor):
 
     @classmethod
     def ie_key(cls):
-        return f'{cls.__module__}.{cls.__name__}'
+        return cls.IE_NAME
+
+
+class ExtractorPlugin:
+
+    EXTRACTOR_PEQN_ATTR = '_dl_plus_peqn'
+
+    def __init__(self, import_path):
+        try:
+            self._base_peqn = PEQN.from_plugin_import_path(import_path)
+        except ValueError as exc:
+            raise ExtractorPluginError(exc)
+        self._extractors = {}
+
+    def export(self, extractor_cls_or_name, name=None):
+        if isinstance(extractor_cls_or_name, str):
+            return partial(self.export, name=extractor_cls_or_name)
+        extractor_cls = extractor_cls_or_name
+        if not issubclass(extractor_cls, Extractor):
+            raise ExtractorPluginError(
+                f'Extractor subclass expected, got: {extractor_cls!r}')
+        if self.EXTRACTOR_PEQN_ATTR in extractor_cls.__dict__:
+            peqn = extractor_cls.__dict__[self.EXTRACTOR_PEQN_ATTR]
+            raise ExtractorPluginError(
+                f'the extractor {extractor_cls!r} is already exported '
+                f'as "{peqn}"'
+            )
+        if name is None:
+            if self._extractors:
+                raise ExtractorPluginError(
+                    'the unnamed extractor must be the only extractor '
+                    'in the plugin'
+                )
+        elif name in self._extractors:
+            raise ExtractorPluginError(
+                f'the plugin already exports an extractor called "{name}"')
+        peqn = self._base_peqn.copy(name=name)
+        setattr(extractor_cls, self.EXTRACTOR_PEQN_ATTR, peqn)
+        extractor_cls.IE_NAME = str(peqn)
+        self._extractors[name] = extractor_cls
+        return extractor_cls
+
+    def get_extractor(self, name=None):
+        return self._extractors[name]
+
+    def get_all_extractors(self):
+        return list(self._extractors.values())
