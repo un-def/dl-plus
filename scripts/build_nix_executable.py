1c12cfff21c56683ee28af4abf2532060dd3505c
un.def <un.def@ya.ru>
2020-12-04 01:16:40 +0300

    Add zipapp build script

---
+++ scripts/build_nix_executable.py
@@ -0,0 +1,16 @@
+import os
+import zipapp
+from pathlib import Path
+
+repo_dir = Path(__file__).parent.parent
+os.chdir(repo_dir)
+
+dest_dir = Path('dist')
+os.makedirs(dest_dir, exist_ok=True)
+zipapp.create_archive(
+    source='src',
+    target=dest_dir / 'dl-plus',
+    interpreter='/usr/bin/env python3',
+    main='dl_plus.cli:main',
+    compressed=True,
+)
